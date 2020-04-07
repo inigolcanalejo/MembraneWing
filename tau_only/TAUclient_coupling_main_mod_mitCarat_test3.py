e259fe0a3f7ab67739185af9c916b60861ad547d
Inigo Lopez <inigolcanalejo@gmail.com>
2020-04-07 14:35:49 +0200

    cleaning script

--- tau_only/TAUclient_coupling_main_mod_mitCarat_test3.py
+++ tau_only/TAUclient_coupling_main_mod_mitCarat_test3.py
@@ -29,65 +29,42 @@ import PyPrep
 import PySurfDeflect
 import PySolv
 import PyDataSet
-from tau_python import tau_mpi_rank
-from tau_python import tau_mpi_nranks
 from tau_python import *
-from tau_python import tau_msg
-rank = tau_mpi_rank()
 
 # Definition of the parameter file
 para_path='/media/inigo/10740FB2740F9A1C/simulations/MembraneWing/tau_only/airfoil_Structured.cntl'
 para_path_mod = para_path + ".mod"
 shutil.copy(para_path, para_path_mod)
 
-#-------------------------------------------------------------------------------
 # Init Tau python classes
-#-------------------------------------------------------------------------------
 Para = PyPara.Parafile(para_path_mod)
-Deform = PyDeform.Deformation(para_path_mod)
 Prep = PyPrep.Preprocessing(para_path_mod)
 Solver = PySolv.Solver(para_path_mod)
-DataSetList = PyDataSet.DataSetList()
 
 grid = Para.get_para_value("Primary grid filename") # Primary grid filename
 #n_outer = int(Para.get_para_value('Unsteady physical time steps'))
 
-#------------------------------------------------------------------
 # Prep + DataSet
-#-------------------------------------------------------------------
 # Read Parameter file with Para already done
 # preprocessing to create dual grid structure
-Prep.run(write_dualgrid=1,free_primgrid=False)
+Prep.run(write_dualgrid=0,free_primgrid=False)
 
 tau_parallel_sync()
-#-----------------------------------------------------------------
 # solve
-#-----------------------------------------------------------------
-this_step_out=0
-n_outer_in=1 # Number of iterations for one coupling iteration
-n_outer_out=1
-fluidIter=50
-this_step=0
-
 Solver.init(verbose = 1, reset_steps = True, n_time_steps = 1) # flow solver  print "time step check out = %d"% this_step_out
 Solver.outer_loop()
-Solver.output()
+# Solver.output()
 tau_plt_init_tecplot_params(para_path_mod)
 tau_solver_write_output_conditional()
 tau_parallel_sync()
 print 'Solve ok'
 tau_parallel_sync()
-time.sleep(20)
-
-DataSetList.write_output()
 
 tau_parallel_sync()
 Solver.finalize()
 tau_free_dualgrid()
 tau_free_prims()
 Para.free_parameters()
-
-DataSetList.free_data()
 
 '''
       Solver.output()
