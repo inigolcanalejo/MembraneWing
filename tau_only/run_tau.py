ba5ca4e37a3bd1e91ceacffcdb8702abe2843fbc
Inigo Lopez <inigolcanalejo@gmail.com>
2020-04-07 18:05:37 +0200

    adding pyprep

--- tau_only/run_tau.py
+++ tau_only/run_tau.py
@@ -1,5 +1,6 @@
 # import modules for TAU
 import PyPara
+import PyPrep
 import PySolv
 
 # Definition of the parameter file
@@ -7,8 +8,10 @@ para_path='/media/inigo/10740FB2740F9A1C/simulations/MembraneWing/tau_only/airfo
 
 # Init Tau python classes
 Para = PyPara.Parafile(para_path)
+Prep = PyPrep.Preprocessing(para_path)
 Solver = PySolv.Solver(para_path)
 
+Prep.run(write_dualgrid=1,free_primgrid=False)
 Solver.init(verbose = 1, reset_steps = True, n_time_steps = 1)
 Solver.outer_loop()
 Solver.output()
