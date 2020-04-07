c1e6dbb21be702c9d5ce05ecfcc54bdee8d9e081
Inigo Lopez <inigolcanalejo@gmail.com>
2020-04-07 16:20:14 +0200

    cleaning joc script

--- tau_only/Job_Script_coupling.sh
+++ tau_only/Job_Script_coupling.sh
@@ -43,9 +43,7 @@ Tautoplt=$Work_Dir/Tautoplt.cntl
 rm -r -f Output_Files
 rm -r -f Ergebnisse
 
-
 #Parameterdatei
-
 sed 's|'"Primary grid filename: TBD"'|'"Primary grid filename: $Grid_File"'|g' -i /$Para_Path
 sed 's|'"Output files prefix: TBD"'|'"Output files prefix: $Output/airfoilSol"'|g' -i /$Para_Path
 sed 's|'"Maximal time step number: TBD"'|'"Maximal time step number: $Maximal_step_number"'|g' -i /$Para_Path
@@ -56,7 +54,6 @@ sed 's|'"Angle alpha (degree): TBD"'|'"Angle alpha (degree): $AOA"'|g' -i /$Para
 sed 's|'"Unsteady physical time step size: TBD"'|'"Unsteady physical time step size: $Step_size"'|g' -i /$Para_Path
 
 #Kopplungsdatei
-
 sed 's|'"para_path='TBD'"'|'"para_path='$Para_Path'"'|g' -i /$Kopl
 sed 's|'"n_outer_out=TBD"'|'"n_outer_out=$n_outer_out"'|g' -i /$Kopl
 sed 's|'"fluidIter=TBD"'|'"fluidIter=$fluidIter"'|g' -i /$Kopl
@@ -70,7 +67,6 @@ sed 's|'"for line in fileinput.input('TBD',inplace=1):"'|'"for line in fileinput
 sed 's|'"List_file = Functions.findFile('TBD')"'|'"List_file = Functions.findFile('$Output/*.plt')"'|g' -i /$Kopl
 
 #Functions_mod
-
 sed 's|'"para_path= 'TBD'"'|'"para_path= '$Para_Path'"'|g' -i /$Funct
 sed 's|'"        if file.startswith('TBD/airfoilSol.MEMBRANE_i=%d'%this_step_out):"'|'"        if file.startswith('$Output/airfoilSol.MEMBRANE_i=%d'%this_step_out):"'|g' -i /$Funct
 
@@ -79,21 +75,14 @@ sed 's|'"Primary grid filename: TBD"'|'"Primary grid filename: $Grid_File.def.1"
 sed 's|'"Boundary mapping filename: TBD"'|'"Boundary mapping filename: $Para_Path"'|g' -i /$Tautoplt
 sed 's|'"Restart-data prefix: TBD"'|'"Restart-data prefix: $Work_Dir/Ergebnisse/Outputs/airfoilSol.pval.unsteady_i=1_t=5.000e-03"'|g' -i /$Tautoplt
 #**********************************
-
 mkdir Outputs
+source /home/inigo/software/pyEmpire/EMPIRE-Core/etc/bashrc.sh ICC
 
-#FSI beginnen
+mpirun -n 1  /home/inigo/software/carat/src/carat CARAT/caratInput_FSI > log_Carat_neu_1 &
 
+sleep 10s
 
-#source /home/inigo/software/pyEmpire/EMPIRE-Core/etc/bashrc.sh ICC
-
-#cd /home/inigo/software/TAU/tau_only/
-
-# mpirun -n 1  /home/inigo/software/carat/src/carat CARAT/caratInput_FSI > log_Carat_neu_1 &
-
-# sleep 10s
-
-# mpirun -n 1  python /home/inigo/software/taubin_svn19618.OPENMPI1.6.4_Python2.7.5/taubin_svn19618.OPENMPI1.6.4_Python2.7.5/taubin_svn19618.OPENMPI1.6.4_Python2.7.5/bin/py_turb1eq/tau.py TAUclient_coupling_main_mod_mitCarat_test3.py airfoil_Structured.cntl log_TAU_neu_1.out
+mpirun -n 1  python /home/inigo/software/taubin_svn19618.OPENMPI1.6.4_Python2.7.5/taubin_svn19618.OPENMPI1.6.4_Python2.7.5/taubin_svn19618.OPENMPI1.6.4_Python2.7.5/bin/py_turb1eq/tau.py TAUclient_coupling_main_mod_mitCarat_test3.py airfoil_Structured.cntl log_TAU_neu_1.out
 
 #**************************************
 
@@ -101,61 +90,56 @@ mkdir Outputs
 
 #***************************************
 
-# mkdir Ergebnisse
-# mkdir Output_Files
+mkdir Ergebnisse
+mkdir Output_Files
 
-# mv $Work_Dir/walldistances_matrix $Work_Dir/Output_Files
-# mv $Work_Dir/xp $Work_Dir/Output_Files
-# mv $Work_Dir/xpNode $Work_Dir/Output_Files
-# mv $Work_Dir/deformation_file.tec  $Work_Dir/Output_Files
-# mv $Work_Dir/ElemTable  $Work_Dir/Output_Files
-# mv $Work_Dir/interface_deformfile.nc $Work_Dir/Output_Files
-# mv $Work_Dir/ping.port $Work_Dir/Output_Files
-# mv $Work_Dir/RBF_matrix $Work_Dir/Output_Files
-# mv $Work_Dir/sta_geo_nonlin_load_disp_curve.dat $Work_Dir/Output_Files
-# mv $Work_Dir/log_Carat_neu_1 $Work_Dir/Ergebnisse
-# mv $Work_Dir/log_TAU_neu_1.out.Python-Interface.stderr $Work_Dir/Ergebnisse
-# mv $Work_Dir/log_TAU_neu_1.out.Python-Interface.stdout $Work_Dir/Ergebnisse
-# mv $Work_Dir/out.err $Work_Dir/Ergebnisse
-# mv $Work_Dir/out.log $Work_Dir/Ergebnisse
-# mv $Work_Dir/Outputs $Work_Dir/Ergebnisse
+mv $Work_Dir/walldistances_matrix $Work_Dir/Output_Files
+mv $Work_Dir/xp $Work_Dir/Output_Files
+mv $Work_Dir/xpNode $Work_Dir/Output_Files
+mv $Work_Dir/deformation_file.tec  $Work_Dir/Output_Files
+mv $Work_Dir/ElemTable  $Work_Dir/Output_Files
+mv $Work_Dir/interface_deformfile.nc $Work_Dir/Output_Files
+mv $Work_Dir/ping.port $Work_Dir/Output_Files
+mv $Work_Dir/RBF_matrix $Work_Dir/Output_Files
+mv $Work_Dir/sta_geo_nonlin_load_disp_curve.dat $Work_Dir/Output_Files
+mv $Work_Dir/log_Carat_neu_1 $Work_Dir/Ergebnisse
+mv $Work_Dir/log_TAU_neu_1.out.Python-Interface.stderr $Work_Dir/Ergebnisse
+mv $Work_Dir/log_TAU_neu_1.out.Python-Interface.stdout $Work_Dir/Ergebnisse
+mv $Work_Dir/out.err $Work_Dir/Ergebnisse
+mv $Work_Dir/out.log $Work_Dir/Ergebnisse
+mv $Work_Dir/Outputs $Work_Dir/Ergebnisse
 
-# /home/inigo/software/taubin_svn19618.OPENMPI1.6.4_Python2.7.5/taubin_svn19618.OPENMPI1.6.4_Python2.7.5/taubin_svn19618.OPENMPI1.6.4_Python2.7.5/bin/tau2plt Tautoplt.cntl
-#Tautoplt.cntl muss bei änderung von n_outer_out angepasst werden!!!!!!!
+/home/inigo/software/taubin_svn19618.OPENMPI1.6.4_Python2.7.5/taubin_svn19618.OPENMPI1.6.4_Python2.7.5/taubin_svn19618.OPENMPI1.6.4_Python2.7.5/bin/tau2plt Tautoplt.cntl
+Tautoplt.cntl muss bei änderung von n_outer_out angepasst werden!!!!!!!
 
-# #Parameterdatei
+#Parameterdatei
+sed 's|'"Primary grid filename: $Grid_File.*"'|'"Primary grid filename: TBD"'|g' -i /$Para_Path
+sed 's|'"Output files prefix: $Output/airfoilSol"'|'"Output files prefix: TBD"'|g' -i /$Para_Path
+sed 's|'"Maximal time step number: $Maximal_step_number"'|'"Maximal time step number: TBD"'|g' -i /$Para_Path
+sed 's|'"Minimum residual: $Minimum_residual"'|'"Minimum residual: TBD"'|g' -i /$Para_Path
+sed 's|'"Unsteady physical time steps: $Unsteady_physical_time_steps"'|'"Unsteady physical time steps: TBD"'|g' -i /$Para_Path
+sed 's|'"Unsteady inner iterations per time step: $Unsteady_inner_iterations_per_time_step"'|'"Unsteady inner iterations per time step: TBD"'|g' -i /$Para_Path
+sed 's|'"Angle alpha (degree): $AOA"'|'"Angle alpha (degree): TBD"'|g' -i /$Para_Path
+sed 's|'"Unsteady physical time step size: $Step_size"'|'"Unsteady physical time step size: TBD"'|g' -i /$Para_Path
 
-# sed 's|'"Primary grid filename: $Grid_File.*"'|'"Primary grid filename: TBD"'|g' -i /$Para_Path
-# sed 's|'"Output files prefix: $Output/airfoilSol"'|'"Output files prefix: TBD"'|g' -i /$Para_Path
-# sed 's|'"Maximal time step number: $Maximal_step_number"'|'"Maximal time step number: TBD"'|g' -i /$Para_Path
-# sed 's|'"Minimum residual: $Minimum_residual"'|'"Minimum residual: TBD"'|g' -i /$Para_Path
-# sed 's|'"Unsteady physical time steps: $Unsteady_physical_time_steps"'|'"Unsteady physical time steps: TBD"'|g' -i /$Para_Path
-# sed 's|'"Unsteady inner iterations per time step: $Unsteady_inner_iterations_per_time_step"'|'"Unsteady inner iterations per time step: TBD"'|g' -i /$Para_Path
-# sed 's|'"Angle alpha (degree): $AOA"'|'"Angle alpha (degree): TBD"'|g' -i /$Para_Path
-# sed 's|'"Unsteady physical time step size: $Step_size"'|'"Unsteady physical time step size: TBD"'|g' -i /$Para_Path
-
-# #Kopplungsdatei
-
-# sed 's|'"para_path='$Para_Path'"'|'"para_path='TBD'"'|g' -i /$Kopl
-# sed 's|'"n_outer_out=$n_outer_out"'|'"n_outer_out=TBD"'|g' -i /$Kopl
-# sed 's|'"fluidIter=$fluidIter"'|'"fluidIter=TBD"'|g' -i /$Kopl
-# sed 's|'"sys.path.append('$Work_Dir')"'|'"sys.path.append('TBD')"'|g' -i /$Kopl
-# sed 's|'"search_text = 'Primary grid filename: $Grid_File'"'|'"search_text = 'Primary grid filename: TBD'"'|g' -i /$Kopl
-# sed 's|'"replace_text = 'Primary grid filename: $Grid_File.def.' + str(this_step_out)"'|'"replace_text = 'Primary grid filename: TBD.def.' + str(this_step_out)"'|g' -i /$Kopl
-# sed 's|'"for line in fileinput.input('$Para_Path',inplace=1):"'|'"for line in fileinput.input('TBD',inplace=1):"'|g' -i /$Kopl
-# sed 's|'"search_text = 'Primary grid filename: $Grid_File.def.' + str(this_step_out-1)"'|'"search_text = 'Primary grid filename: TBD.def.' + str(this_step_out-1)"'|g' -i /$Kopl
-# sed 's|'"replace_text = 'Primary grid filename: $Grid_File.def.' + str(this_step_out)"'|'"replace_text = 'Primary grid filename: TBD.def.' + str(this_step_out)"'|g' -i /$Kopl
-# sed 's|'"for line in fileinput.input('$Para_Path',inplace=1):"'|'"for line in fileinput.input('TBD',inplace=1):"'|g' -i /$Kopl
-# sed 's|'"List_file = Functions.findFile('$Output/*.plt')"'|'"List_file = Functions.findFile('TBD')"'|g' -i /$Kopl
+#Kopplungsdatei
+sed 's|'"para_path='$Para_Path'"'|'"para_path='TBD'"'|g' -i /$Kopl
+sed 's|'"n_outer_out=$n_outer_out"'|'"n_outer_out=TBD"'|g' -i /$Kopl
+sed 's|'"fluidIter=$fluidIter"'|'"fluidIter=TBD"'|g' -i /$Kopl
+sed 's|'"sys.path.append('$Work_Dir')"'|'"sys.path.append('TBD')"'|g' -i /$Kopl
+sed 's|'"search_text = 'Primary grid filename: $Grid_File'"'|'"search_text = 'Primary grid filename: TBD'"'|g' -i /$Kopl
+sed 's|'"replace_text = 'Primary grid filename: $Grid_File.def.' + str(this_step_out)"'|'"replace_text = 'Primary grid filename: TBD.def.' + str(this_step_out)"'|g' -i /$Kopl
+sed 's|'"for line in fileinput.input('$Para_Path',inplace=1):"'|'"for line in fileinput.input('TBD',inplace=1):"'|g' -i /$Kopl
+sed 's|'"search_text = 'Primary grid filename: $Grid_File.def.' + str(this_step_out-1)"'|'"search_text = 'Primary grid filename: TBD.def.' + str(this_step_out-1)"'|g' -i /$Kopl
+sed 's|'"replace_text = 'Primary grid filename: $Grid_File.def.' + str(this_step_out)"'|'"replace_text = 'Primary grid filename: TBD.def.' + str(this_step_out)"'|g' -i /$Kopl
+sed 's|'"for line in fileinput.input('$Para_Path',inplace=1):"'|'"for line in fileinput.input('TBD',inplace=1):"'|g' -i /$Kopl
+sed 's|'"List_file = Functions.findFile('$Output/*.plt')"'|'"List_file = Functions.findFile('TBD')"'|g' -i /$Kopl
 
 #Functions_mod
-
 sed 's|'"para_path= '$Para_Path'"'|'"para_path= 'TBD'"'|g' -i /$Funct
 sed 's|'"        if file.startswith('$Output/airfoilSol.MEMBRANE_i=%d'%this_step_out):"'|'"        if file.startswith('TBD/airfoilSol.MEMBRANE_i=%d'%this_step_out):"'|g' -i /$Funct
 
 #TAUtoplt
-
 sed 's|'"Primary grid filename: $Grid_File.def.1"'|'"Primary grid filename: TBD"'|g' -i /$Tautoplt
 sed 's|'"Boundary mapping filename: $Para_Path"'|'"Boundary mapping filename: TBD"'|g' -i /$Tautoplt
 sed 's|'"Restart-data prefix: $Work_Dir/Ergebnisse/Outputs/airfoilSol.pval.unsteady_i=1_t=5.000e-03"'|'"Restart-data prefix: TBD"'|g' -i /$Tautoplt
-
