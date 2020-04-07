1a691f24c9cb4fa64077d8a1c4bf461fdbd6c9b9
Inigo Lopez <inigolcanalejo@gmail.com>
2020-04-07 14:54:41 +0200

    cleaning

--- tau_only/TAUclient_coupling_main_mod_mitCarat_test3.py
+++ tau_only/TAUclient_coupling_main_mod_mitCarat_test3.py
@@ -9,8 +9,7 @@ para_path='/media/inigo/10740FB2740F9A1C/simulations/MembraneWing/tau_only/airfo
 Para = PyPara.Parafile(para_path)
 Solver = PySolv.Solver(para_path)
 
-# solve
-Solver.init(verbose = 1, reset_steps = True, n_time_steps = 1) # flow solver  print "time step check out = %d"% this_step_out
+Solver.init(verbose = 1, reset_steps = True, n_time_steps = 1)
 Solver.outer_loop()
 # Solver.output()
 Solver.finalize()
