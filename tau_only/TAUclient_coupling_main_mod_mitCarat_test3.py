0b840f4161588172e67deb8af62fa9c427678f8f
Inigo Lopez <inigolcanalejo@gmail.com>
2020-04-07 14:53:21 +0200

    cleaning

--- tau_only/TAUclient_coupling_main_mod_mitCarat_test3.py
+++ tau_only/TAUclient_coupling_main_mod_mitCarat_test3.py
@@ -1,16 +1,13 @@
-import shutil
 # import modules for TAU
 import PyPara
 import PySolv
 
 # Definition of the parameter file
 para_path='/media/inigo/10740FB2740F9A1C/simulations/MembraneWing/tau_only/airfoil_Structured.cntl'
-para_path_mod = para_path + ".mod"
-shutil.copy(para_path, para_path_mod)
 
 # Init Tau python classes
-Para = PyPara.Parafile(para_path_mod)
-Solver = PySolv.Solver(para_path_mod)
+Para = PyPara.Parafile(para_path)
+Solver = PySolv.Solver(para_path)
 
 # solve
 Solver.init(verbose = 1, reset_steps = True, n_time_steps = 1) # flow solver  print "time step check out = %d"% this_step_out
