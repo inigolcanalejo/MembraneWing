0de95ec013457f4a77b35e63d75be9f95774996d
Inigo Lopez <inigolcanalejo@gmail.com>
2020-11-17 16:50:32 +0100

    adding te and le to thrust and torque computation

--- compute_turbine_forces/compute_forces.py
+++ compute_turbine_forces/compute_forces.py
@@ -1,6 +1,3 @@
-output_filename_up = 'airfoilSol.surface.pval.15010.MEMBRANE_UP.dat'
-output_filename_down = 'airfoilSol.surface.pval.15010.MEMBRANE_DOWN.dat'
-
 import re, sys, json, os
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,23 +12,25 @@ sys.path.append(tau_settings["kratos_path"])
 sys.path.append(tau_path + "py_turb1eq/")
 
 # tau_functions can only be imported after appending kratos' path
-import tau_functions_Steady as TauFunctions
+import tau_functions_Steady as TauFunctionsSteady
+import tau_functions as TauFunctions
 
 def ComputeThrustAndTorque(output_filename):
+    print('Computing: ', output_filename)
     # Read output file
-    position_info, mesh_info, nodal_data, elem_connectivities = TauFunctions.ReadInterfaceFile(output_filename)
+    position_info, mesh_info, nodal_data, elem_connectivities = TauFunctionsSteady.ReadInterfaceFile(output_filename)
 
     # Read mesh info
     NodesNr = mesh_info[0]
     print("NodesNr = ", NodesNr)
 
-    X, Y, Z = TauFunctions.SaveCoordinatesList(nodal_data, position_info, NodesNr)
+    X, Y, Z = TauFunctionsSteady.SaveCoordinatesList(nodal_data, position_info, NodesNr)
 
     velocity = 41.09
-    P = TauFunctions.SavePressure(nodal_data, position_info, NodesNr, velocity)
+    P = TauFunctionsSteady.SavePressure(nodal_data, position_info, NodesNr, velocity)
 
     # calculating the force vector
-    fluid_forces = TauFunctions.CalculateNodalFluidForces(X, Y, Z, P, elem_connectivities)
+    fluid_forces = TauFunctionsSteady.CalculateNodalFluidForces(X, Y, Z, P, elem_connectivities)
 
     # list containing "y" coordinate of each section (not ordered)
     section_y = []
@@ -49,7 +48,7 @@ def ComputeThrustAndTorque(output_filename):
         # loop over already found sections
         for j in range(len(section_y)):
             # if distance is below tolerance, the section already exists
-            if abs(Y[i]-section_y[j]) < 2e-3:
+            if abs(Y[i]-section_y[j]) < 1.99e-3:
                 section_found = True
 
                 # add force_x contribution to section thrust
@@ -74,6 +73,7 @@ def ComputeThrustAndTorque(output_filename):
     sorted_section_torque = [i for _,i in sorted(zip(section_y_abs,section_torque))]
     section_y_abs.sort()
 
+    '''
     # compute distance between sections
     delta_y = [0.5*(section_y_abs[1]-section_y_abs[0])]
 
@@ -86,28 +86,51 @@ def ComputeThrustAndTorque(output_filename):
     section_thrust_distribution = []
     for i in range(len(sorted_section_thrust)):
         section_thrust_distribution.append(sorted_section_thrust[i]/delta_y[i])
+    '''
 
     return sorted_section_thrust, sorted_section_torque, section_y_abs
 
 # Main script
+step = 1
+output_file_pattern = 'airfoilSol.pval.deform_i='
+sub_step = 0
+TauFunctions.ChangeFormat(working_path, step, "MEMBRANE_UP", "MEMBRANE_DOWN", output_file_pattern, sub_step)
+TauFunctions.ChangeFormat(working_path, step, "LE_FF", "TE_FF", output_file_pattern, sub_step)
+
+input_filename = 'Outputs/airfoilSol.surface.pval.deform_i=1_t=2.315e-03_subiter=0'
+output_filename_up = input_filename + '.MEMBRANE_UP.dat'
+output_filename_down = input_filename + '.MEMBRANE_DOWN.dat'
+output_filename_le = input_filename + '.LE_FF.dat'
+output_filename_te = input_filename + '.TE_FF.dat'
+
 thrust_up, torque_up, section_y_abs_up = ComputeThrustAndTorque(output_filename_up)
 thrust_down, torque_down, section_y_abs_down = ComputeThrustAndTorque(output_filename_down)
+thrust_le, torque_le, section_y_abs_le = ComputeThrustAndTorque(output_filename_le)
+thrust_te, torque_te, section_y_abs_te = ComputeThrustAndTorque(output_filename_te)
 
 total_thrust = []
 total_torque = []
 
 for i in range(len(thrust_up)):
-    total_thrust.append(thrust_up[i] + thrust_down[i])
-    total_torque.append(torque_up[i] + torque_down[i])
+    total_thrust.append(thrust_up[i] + thrust_down[i] + thrust_le[i] + thrust_te[i])
+    total_torque.append(torque_up[i] + torque_down[i] + torque_le[i] + torque_te[i])
 
 # plotting thrust and torque distributions
 plt.title('Torque and Thrust radial distributions')
 plt.xlabel('Radius [m]')
+
+# Thrust
 # plt.plot(section_y_abs_up, thrust_up, label='Thrust_up [N]')
-# plt.plot(section_y_abs_up, torque_up, label='Torque_up [Nm]')
 # plt.plot(section_y_abs_up, thrust_down, label='Thrust_down [N]')
-# plt.plot(section_y_abs_up, torque_down, label='Torque_down [Nm]')
-plt.plot(section_y_abs_up, total_thrust, label='Thrust_total [N]')
+# plt.plot(section_y_abs_up, thrust_le, label='Thrust_le [N]')
+# plt.plot(section_y_abs_up, thrust_te, label='Thrust_te [N]')
+# plt.plot(section_y_abs_up, total_thrust, label='Thrust_total [N]')
+
+# Torque
+plt.plot(section_y_abs_up, torque_up, label='Torque_up [Nm]')
+plt.plot(section_y_abs_up, torque_down, label='Torque_down [Nm]')
+plt.plot(section_y_abs_up, torque_le, label='Torque_le [Nm]')
+plt.plot(section_y_abs_up, torque_te, label='Torque_te [Nm]')
 plt.plot(section_y_abs_up, total_torque, label='Torque_total [Nm]')
 plt.legend(loc="upper left")
 plt.show()
